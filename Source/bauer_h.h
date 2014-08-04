1d1a232229ea849489489a49f872ceb53410a06b
Bauer <nikolabauver@gmail.com>
2014-08-04 16:51:46 +0200

    Additional define files

---
+++ Source/bauer_h.h
@@ -0,0 +1,22 @@
+//Varijable
+
+float xyz[3][MAX_PLAYERS];
+float Armour[MAX_PLAYERS];
+float Health[MAX_PLAYERS];
+float HeltiIgraca[MAX_PLAYERS];
+float helts[MAX_PLAYERS];
+
+#define VERSION "1.0"
+#define UPDATE "04.08.2014"
+
+char Secured[MAX_PLAYERS];
+char Kickk[MAX_PLAYERS];
+char pip[16];
+char ime[MAX_PLAYER_NAME];
+char NovacStart[MAX_PLAYERS];
+
+
+//Podesavanja
+char RCONZ = 1;
+char FLYH = 1;
+char SPEEDH = 1;
